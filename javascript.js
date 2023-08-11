b4af3a1eea3f3af091567a7e0862ebb29123eb03
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2023-08-11 16:42:40 +0800

    add javascript.js

--- javascript.js
+++ javascript.js
@@ -1,44 +1,8 @@
-function add(a, b) {
-  return a + b;
-}
+const btns = document.querySelectorAll(".btn");
 
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  if (b === 0) return "Are you crazy?!";
-  return a / b;
-}
-
-function operate(a, b, operator) {
-  let result;
-  switch (operator) {
-    case "+":
-      result = add(a, b);
-      break;
-    case "-":
-      result = subtract(a, b);
-      break;
-    case "/":
-      result = divide(a, b);
-      break;
-    case "*":
-      result = multiply(a, b);
-      break;
-    default:
-      return "Unknown operation!";
-  }
-  return result;
-}
-
-// module.exports = {
-//   add,
-//   subtract,
-//   divide,
-//   multiply,
-// };
+btns.forEach((btn) => {
+  btn.addEventListener("click", function onClick() {
+    btn.style.backgroundColor = "salmon";
+    btn.style.color = "white";
+  });
+});
