9cb5ba5f8828dc4898721b394e241b864aca554a
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2023-08-11 18:53:33 +0800

    hover

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
-const btns = document.querySelectorAll(".btn");
+// const btns = document.querySelectorAll(".btn");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", function onClick() {
-    btn.style.backgroundColor = "salmon";
-    btn.style.color = "white";
-  });
-});
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", function onClick() {
+//     btn.style.backgroundColor = "salmon";
+//     btn.style.color = "white";
+//   });
+// });
