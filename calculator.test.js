da973e790c7134f7572eb2532ccb433431e1fc2c
Baraa Basim <34444872+BaraaBasim@users.noreply.github.com>
2023-08-13 01:05:13 +0800

    clean up

--- calculator.test.js
+++ calculator.test.js
@@ -108,25 +108,3 @@ describe("multiply", () => {
 //     expect(calculator.power(4, 3)).toBe(64); // 4 to third power is 64
 //   });
 // });
-
-// describe('factorial', () => {
-//   test('computes the factorial of 0', () => {
-//     expect(calculator.factorial(0)).toBe(1); // 0! = 1
-//   });
-
-//   test('computes the factorial of 1', () => {
-//     expect(calculator.factorial(1)).toBe(1);
-//   });
-
-//   test('computes the factorial of 2', () => {
-//     expect(calculator.factorial(2)).toBe(2);
-//   });
-
-//   test('computes the factorial of 5', () => {
-//     expect(calculator.factorial(5)).toBe(120);
-//   });
-
-//   test('computes the factorial of 10', () => {
-//     expect(calculator.factorial(10)).toBe(3628800);
-//   });
-// });
